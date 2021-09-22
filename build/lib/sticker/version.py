d859838127d100b8997ee9dfa029c9b52f05daed
bobylev.n <bobnikp@gmail.com>
2021-09-22 15:22:58 +0300

    changed HOME and removed dot from path for import

---
+++ build/lib/sticker/version.py
@@ -0,0 +1,6 @@
+# Generated in setup.py
+
+git_tag = None
+git_revision = '71afd5e4'
+version = '0.1.0+dev.71afd5e4'
+linkified_version = '0.1.0+dev.[71afd5e4](https://github.com/maunium/stickerpicker/commit/71afd5e40327d04502350b6efe57f7c831122d12)'
